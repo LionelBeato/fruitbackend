d23c782bab183c53f1d7251cb2f285965dd1b318
LionelBeato <lionel.beato@gmail.com>
2020-02-10 15:57:15 -0500

    tweaks CORS

--- server.js
+++ server.js
@@ -15,11 +15,11 @@ const port = process.env.PORT || 3000
 
 let apples = {name:'apples', amount:4}
 
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-//   });
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 
 app.get('/', (req, res) => res.send('hello there!'))
