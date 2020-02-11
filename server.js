4c2788b46ceb4de9cb37061dd46afdd3a70121b5
LionelBeato <lionel.beato@gmail.com>
2020-02-11 16:39:31 -0500

    adds more header

--- server.js
+++ server.js
@@ -20,6 +20,8 @@ const { buildSchema } = require('graphql');
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "https://lionelbeato.github.io"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "DELETE, POST, GET, OPTIONS")
+
     next();
   });
 
