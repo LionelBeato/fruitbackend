74bc71ac7e3c5bdb0e58085ccc15b1dd5c85dde8
LionelBeato <lionel.beato@gmail.com>
2020-02-11 15:19:52 -0500

    refactors to use graphql

--- server.js
+++ server.js
@@ -12,18 +12,32 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+const graphqlHTTP = require('express-graphql');
+const { buildSchema } = require('graphql');
 
-let apples = {name:'apples', amount:4}
+// let apples = {name:'apples', amount:4}
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+//   });
 
 
-app.get('/', (req, res) => res.send('hello there!'))
-// app.get('/index', (req, res) => res.sendFile('index.html', {root:__dirname}))
-app.get('/data', (req, res) => res.json(apples))
-app.use(express.static('./'))
-app.listen(port, () => console.log(`Example port on port ${port}`))
+// app.get('/', (req, res) => res.send('hello there!'))
+// // app.get('/index', (req, res) => res.sendFile('index.html', {root:__dirname}))
+// app.get('/data', (req, res) => res.json(apples))
+// app.use(express.static('./'))
+// app.listen(port, () => console.log(`Example port on port ${port}`))
+
+
+
+let root = { hello: () => 'Hello world!' };
+
+let app = express();
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+app.listen(4000, () => console.log('Now browse to localhost:3000/graphql'));
