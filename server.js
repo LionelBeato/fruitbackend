781d14c066c8e761ad0e5fe169375ad8c9f72cf6
LionelBeato <lionel.beato@gmail.com>
2020-02-11 16:33:49 -0500

    adds explicit url to CORS header

--- server.js
+++ server.js
@@ -17,11 +17,11 @@ const { buildSchema } = require('graphql');
 
 // let apples = {name:'apples', amount:4}
 
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-//   });
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://lionelbeato.github.io/fruitfrontend/"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 
 // app.get('/', (req, res) => res.send('hello there!'))
